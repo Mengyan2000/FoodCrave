82db1d7ff4bb3c54de52a0a74269a9cdd8d7ae01
mengyan2000 <mengyan.liu2000@gmail.com>
2020-05-04 12:34:03 +0800

    Chunk

--- app/src/main/java/com/example/FoodCrave/ResultActivity.java
+++ app/src/main/java/com/example/FoodCrave/ResultActivity.java
@@ -3,6 +3,10 @@ package com.example.FoodCrave;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
 import java.util.ArrayList;
 
 public class ResultActivity extends AppCompatActivity {
@@ -23,8 +27,21 @@ public class ResultActivity extends AppCompatActivity {
         for (int i = 0; i < resultNames.size(); i++) {
             System.out.println(resultNames.get(i));
         }
-        setContentView(R.layout.activity_result);
 
+
+        LinearLayout restaurant = findViewById(R.id.restaurants_list);
+
+
+        for (int i = 0; i < resultNames.size(); i++) {
+            String name = resultNames.get(i);
+            View chunk = getLayoutInflater().inflate(R.layout.chunk_restaurant, restaurant, false);
+
+            TextView single = chunk.findViewById(R.id.gameOwner);
+            single.setText(name);
+            restaurant.addView(chunk);
+        }
+
+        setContentView(R.layout.activity_result);
     }
 
     // When back button is pressed, go back to SearchActivity
